425ef7a54357395a3e941bae0b9f86ecb536ed38
sergio884 <edilberto.sergio.valle.ortiz@gmail.com>
2021-10-17 13:21:40 -0500

    Autenticacion por correo

---
+++ app/src/main/java/com/example/logint/SignInActivity.kt
@@ -0,0 +1,84 @@
+package com.example.logint
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.logint.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding : ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty()->{
+                    Toast.makeText(baseContext, "Porfavor de poner el correo y contraseña",Toast.LENGTH_SHORT).show()
+                }else->{
+                    signIn(mEmail,mPassword)
+                }
+            }
+        }
+
+        binding.signUpTextView.setOnClickListener {
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
+        binding.recoveryAccountTextView.setOnClickListener {
+            val intent = Intent(this, AccountRecoveryActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            if(currentUser.isEmailVerified){
+                reload()
+            }else{
+                val intent = Intent(this,CheckEmail::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun signIn (email : String , password : String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    val user = auth.currentUser
+                    reload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun reload(){
+        val intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
+    }
+}
