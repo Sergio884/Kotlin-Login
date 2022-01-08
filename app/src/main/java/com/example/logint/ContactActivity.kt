7b74aa10601ef1f6e9a08b9148229db99651ace3
Kevin Edwin Sandoval <ksandoval@tecnocengroup.com>
2022-01-08 16:06:29 -0600

    Actualización de contactos

--- app/src/main/java/com/example/logint/ContactActivity.kt
+++ app/src/main/java/com/example/logint/ContactActivity.kt
@@ -3,6 +3,8 @@ package com.example.logint
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,66 +15,90 @@ import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.core.content.ContextCompat
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.jar.Manifest
 
-class ContactActivity : AppCompatActivity() {
+
+class ContactActivity : AppCompatActivity(){
+    private var auth = Firebase.auth
+    private val db = FirebaseFirestore.getInstance()
+    private var key = 1
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
         var nombreUsuario: TextView = findViewById(R.id.name_contact)
         var numberContact: TextView = findViewById(R.id.number_contact)
-        var addContacts: ImageView = findViewById(R.id.add_contacts)
-        val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){
-                isGranted: Boolean ->
-            if (isGranted){
+        val addContacts: ImageView = findViewById(R.id.add_contacts)
 
-                Log.i("Bien","Bien")
-            }else{
-                Log.i("Mal","Mal")
-            }
-        }
+
+
+
+        // Launch para seleccionar a el contacto
         val contactosLauncher = registerForActivityResult(StartActivityForResult()) { activityResult ->
 
             if (activityResult.resultCode == RESULT_OK) {
                 val data: Intent? = activityResult.data
-                val contactUri: Uri = data?.data!!
-                val projection: Array<String> = arrayOf(CommonDataKinds.Phone.CONTENT_TYPE)
-                contentResolver.query(contactUri, projection, null, null, null).use { cursor ->
-                    // If the cursor returned is valid, get the phone number
-                    if (cursor!!.moveToFirst()) {
-                        val numberIndex = cursor.getColumnIndex(CommonDataKinds.Phone.NUMBER)
-                        val number = cursor.getString(numberIndex)
+                val cursor1: Cursor
+                val cursor2: Cursor
+
+                //get data from intent
+                val uri = data!!.data
+
+                cursor2 = contentResolver.query(uri!!, null, null, null, null)!!
+                if (cursor2.moveToFirst()){
+                    val user = auth.currentUser
+                    val indexNombreContacto = cursor2.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    val nombreContactos= cursor2.getString(indexNombreContacto)
+                    val contactId = cursor2.getString(cursor2.getColumnIndex(ContactsContract.Contacts._ID))
+                    cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+contactId, null, null)!!
+                    if (cursor1.moveToFirst()){
+                        val indexNumberContacto = cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                        val number = cursor1.getString(indexNumberContacto)
                         Toast.makeText(
                             this@ContactActivity,
-                            number,
+                            "Nombre: ${nombreContactos} Numero: ${number}",
                             Toast.LENGTH_SHORT
                         ).show()
+                        db.collection("contacts").document("${number}").set(
+                            hashMapOf("name" to nombreContactos,"emailUser" to "pepito@gmail.com")
+                        )
                     }
+
+
+                    cursor1.close()
+                    cursor2.close()
                 }
+            }
+            else{
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
             }
         }
         addContacts.setOnClickListener {
-
-            val intentAdd = Intent(Intent.ACTION_PICK).apply {
-                type = CommonDataKinds.Phone.CONTENT_TYPE
+            if(checkContactPermission()){
+               // val intentAdd = Intent (Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+                val intentAdds = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+                //startActivityForResult(intentAdds, 2)
+                contactosLauncher.launch(intentAdds)
             }
-            if(intentAdd.resolveActivity(packageManager)!=null){
-                Toast.makeText(
-                    this@ContactActivity,
-                    "SI DIOOOO",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                contactosLauncher.launch(intent)
-            }
-
-            //.apply {type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE }
 
         }
 
     }
+    //Verificar si los permisos de READ_CONTACTS estan activados
+    private fun checkContactPermission(): Boolean{
+        return  ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
-
+    //Menu inflater
     fun showPopUp(v: View) {
         PopupMenu(this, v).apply {
             inflate(R.menu.menu_contactos)
@@ -99,6 +125,52 @@ class ContactActivity : AppCompatActivity() {
             }
         }.show()
     }
+
+    /*
+    @SuppressLint("Range")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //handle intent results || calls when user from Intent (Contact Pick) picks or cancels pick contact
+        if (resultCode == RESULT_OK){
+
+            //calls when user click a contact from contacts (intent) list
+            if (requestCode == 2){
+
+                val cursor1: Cursor
+                val cursor2: Cursor
+
+                //get data from intent
+                val uri = data!!.data
+
+                cursor2 = contentResolver.query(uri!!, null, null, null, null)!!
+                if (cursor2.moveToFirst()){
+
+                    val indexNombreContacto = cursor2.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    val nombreContactos= cursor2.getString(indexNombreContacto)
+                    val contactId = cursor2.getString(cursor2.getColumnIndex(ContactsContract.Contacts._ID))
+                    cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+contactId, null, null)!!
+                    if (cursor1.moveToFirst()){
+                        val indexNumberContacto = cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                        val number = cursor1.getString(indexNumberContacto)
+                        Toast.makeText(
+                            this@ContactActivity,
+                            "Nombre: ${nombreContactos} Numero: ${number}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+
+                    cursor1.close()
+                    cursor2.close()
+                }
+            }
+
+        }
+        else{
+            //cancelled picking contact
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    } */
 
 
 
