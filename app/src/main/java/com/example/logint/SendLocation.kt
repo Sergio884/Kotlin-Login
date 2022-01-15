2c0f42a90361af0ff0ef3f92aeec56ad2398f952
Kevin Edwin Sandoval <ksandoval@tecnocengroup.com>
2022-01-15 01:28:06 -0600

    Holaaaaaaa

--- app/src/main/java/com/example/logint/SendLocation.kt
+++ app/src/main/java/com/example/logint/SendLocation.kt
@@ -18,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.HandlerThread
 import android.os.Looper
+import android.os.SystemClock.sleep
 import android.provider.Settings
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -26,53 +27,41 @@ import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import com.google.android.gms.location.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.delay
 import java.util.*
 import java.util.logging.Handler
 
 class SendLocation : Service() {
     lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     var estado=true
-    lateinit var location: Location
+    //lateinit var location: Location
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-      /*  if(checkPermissions()){
-            if(isLocationEnabled()){
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-                }
-            }
-        }else{
-
-        }*/
-        val hilo = Hilo(this)
-        hilo.start()
-
-
+       val hilo = Hilo(this)
+       hilo.start()
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
     }
 
-    class Hilo(p:SendLocation,):Thread(){
-
+    class Hilo(p:SendLocation):Thread(){
+        private lateinit var locationCallback: LocationCallback
         var pun = p
         var i = 0
 
         override fun run(){
             super.run()
-            startLocationUpdates()
+            //startLocationUpdates()
             while(i<10){
                 sleep(5000)
-
+                pun.mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(pun)
                 if (ActivityCompat.checkSelfPermission(
                         pun,
                         Manifest.permission.ACCESS_FINE_LOCATION
@@ -91,18 +80,26 @@ class SendLocation : Service() {
                     return
                 }
                 else{
+                    val mLocationRequest = LocationRequest()
+                    mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                    mLocationRequest.interval = 0
+                    mLocationRequest.fastestInterval = 0
+                    mLocationRequest.numUpdates = 1
+                    pun.mFusedLocationProviderClient.flushLocations()
+                    pun.mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallBack, Looper.getMainLooper())
+
                     pun.mFusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
                         val location= task.result
                         if(location!=null){
                            // requestNewLocationData()
                            println("Latitudd = ${location.latitude} Longitud = ${location.longitude}")
-                            val database = Firebase.database
+                           /* val database = Firebase.database
                             val reference = database.getReference("reminders")
                             val key = reference.push().key
                                 if(key != null){
-                                    val reminder = Reminder(key, location.latitude,location.longitude) //Objeto de la base de datos
+                                    val reminder = Reminder(key, location.latitude,location.longitude)
                                     reference.child(key).setValue(reminder)
-                                }
+                                } */
                             i++
                             }
                         else{
@@ -161,13 +158,7 @@ class SendLocation : Service() {
             override fun onLocationResult(locationResult: LocationResult) {
                 val location : Location = locationResult.lastLocation
                 println("Latituds = ${location.latitude} Longitud = ${location.longitude}")
-                val database = Firebase.database
-                val reference = database.getReference("reminders")
-                val key = reference.push().key
-                if(key != null){
-                    val reminder = Reminder(key, location.latitude,location.longitude) //Objeto de la base de datos
-                    reference.child(key).setValue(reminder)
-                }
+
 
             }
         }
