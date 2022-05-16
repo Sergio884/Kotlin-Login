041968a270a7d32de93a823b2a2a2413613069f9
Kevin Edwin Sandoval <ksandoval@tecnocengroup.com>
2022-05-16 01:28:08 -0500

    OnRoute

---
+++ app/src/main/java/com/example/logint/OnRouteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnRouteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_route)
+
+    }
+}
